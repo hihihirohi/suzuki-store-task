8b9e0616639682dc732c6aa0f26db56f62a3352c
hihihirohi <hirothugu326@gmail.com>
2019-11-25 17:43:06 +0900

    ショッピングカートの作成

---
+++ angular-frontend/src/app/cart/cart.component.ts
@@ -0,0 +1,19 @@
+import { CartService } from '../cart.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items;
+
+  constructor(
+    private cartService: CartService
+  ) { }
+
+  ngOnInit() {
+    this.items = this.cartService.getItems();
+  }
+}
