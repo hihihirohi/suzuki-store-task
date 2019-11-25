780fe35f80e7e7087593ca9b6961a2535c70d771
hihihirohi <hirothugu326@gmail.com>
2019-11-25 16:08:27 +0900

    ファイルの入れ替え

--- angular-frontend/src/app/top-bar/top-bar.component.ts
+++ angular-frontend/src/app/top-bar/top-bar.component.ts
@@ -13,3 +13,10 @@ export class TopBarComponent implements OnInit {
   }
 
 }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
